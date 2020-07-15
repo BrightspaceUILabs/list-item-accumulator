cfec66511468ebf6f8db1ee2c7ad0322ca6d6aa3
Zina Ramirez <instinctualart@gmail.com>
2020-07-15 16:47:17 -0400

    Add basic lang setup, trying to force a release (#7)
    * Adding lang setup
    
    * Added comments
    
    * Add lang to package
    
    * Forcing update
    
    * Forcing release

---
+++ lang/locales/en.js
@@ -0,0 +1,7 @@
+/* eslint quotes: ["error", "double"] */
+export const en = {
+	"actions": "Actions", // Label for the actions dropdown
+	"moveDown": "Move Down", // Reorder an item down one
+	"moveUp": "Move Up", // Reorder an item up one
+	"secondaryActions": "Secondary actions" // Label for the secondary actions menu
+};
