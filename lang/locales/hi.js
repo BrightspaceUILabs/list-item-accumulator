650d04e5c96e274ec6fda4d73d88f0ce100b38f5
JenkinsReader <resupport@desire2learn.com>
2022-02-24 18:48:43 -0500

    Translation Updates

---
+++ lang/locales/hi.js
@@ -0,0 +1,7 @@
+/* eslint quotes: ["error", "double"] */
+export default {
+	"actions": "क्रियाएँ", // Label for the actions dropdown
+	"moveDown": "नीचे ले जाएँ", // Reorder an item down one
+	"moveUp": "ऊपर ले जाएँ", // Reorder an item up one
+	"secondaryActions": "द्वितीयक क्रियाएँ" // Label for the secondary actions menu
+};
