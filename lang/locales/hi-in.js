5def2f114874ace70ae80b998b9ceca6359b8812
Nidhi-github1994 <61367359+Nidhi-github1994@users.noreply.github.com>
2022-03-08 15:37:51 -0500

    Add files via upload

---
+++ lang/locales/hi-in.js
@@ -0,0 +1,7 @@
+/* eslint quotes: ["error", "double"] */
+export default {
+	"actions": "कार्रवाई", // Label for the actions dropdown
+	"moveDown": "नीचे की ओर", // Reorder an item down one
+	"moveUp": "बढ़ाना", // Reorder an item up one
+	"secondaryActions": "माध्यमिक क्रियाएं" // Label for the secondary actions menu
+};
