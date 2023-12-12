4bbd0639d43fc11c8d85dfae7ba6d7bcffe67f6a
Dave Lockhart <dlockhart@users.noreply.github.com>
2023-12-12 09:25:44 -0500

    fix: lit dependencies (#67)

--- demo/demo-list-usage.js
+++ demo/demo-list-usage.js
@@ -1,10 +1,9 @@
 import '../list-item-accumulator.js';
 import '@brightspace-ui/core/components/list/list.js';
 import '@brightspace-ui/core/components/menu/menu-item.js';
-import { css, html, LitElement } from 'lit-element/lit-element.js';
-import { ifDefined } from 'lit-html/directives/if-defined.js';
-import { nothing } from 'lit-html';
-import { repeat } from 'lit-html/directives/repeat.js';
+import { css, html, LitElement, nothing } from 'lit';
+import { ifDefined } from 'lit/directives/if-defined.js';
+import { repeat } from 'lit/directives/repeat.js';
 
 class DemoAccumulatorUsage extends LitElement {
 	static get properties() {
